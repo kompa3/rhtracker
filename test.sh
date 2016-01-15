7de564c317443031cc32bb177d3dac5614c5beca
Tommi Palomäki <tommi.palomaki@cybercom.com>
2016-01-15 13:46:40 +0000

    Publish to AWS IoT

--- test.sh
+++ test.sh
@@ -5,5 +5,5 @@ set -ex
 # See https://github.com/logstash-plugins/logstash-output-rabbitmq/issues/30
 
 sudo /opt/logstash/bin/logstash -f data-collector.conf < test-data.txt
-sudo /opt/logstash/bin/logstash -f data-processing.conf
+sudo /opt/logstash/bin/logstash -f data-processing.conf --debug
 
