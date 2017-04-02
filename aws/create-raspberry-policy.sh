d6a0df9e4b14c75f7cf1c8e0c75b4d109f1978a7
kompa3 <kompa3@gmail.com>
2017-04-02 14:36:35 +0000

    Refactored to use Zabbix and init.d service

--- aws/create-raspberry-policy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -ex
-
-# Create the universal raspberry.policy which will be attached to all Raspberry thing certificates
-aws iot create-policy --policy-name raspberry.policy --policy-document file://raspberry.policy
